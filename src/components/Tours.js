9ecd099db19b7f818598932ae3025e171a68fb06
Aditya-000 <adityasingh.singh504@gmail.com>
2023-08-07 13:21:02 +0530

    Initial Commit

---
+++ src/components/Tours.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+
+function Tours({tours,removetour}){
+    return(
+     <div className="container">
+          <div>
+            <h2 className="title"> Plan With Aditya </h2>
+          </div>
+          <div className="cards">
+             {
+                tours.map( (tour) =>{
+                    return <Card key={tour.id} {...tour}  removetour ={removetour}></Card>
+                } )
+             }
+          </div>
+
+     </div>
+    );
+
+}
+
+export default Tours;
